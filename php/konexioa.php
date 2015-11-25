3ff6aa420ca64c638f102c0446f2a1bda377554b
Jon Egana <joneganagranado@gmail.com>
2015-11-25 08:11:04 +0100

    commit2

---
+++ php/konexioa.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'config.php';
+$dblink = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+	if(!mysqli_connect_errno()) {
+		//echo 'Konektatut da';
+	}else{
+		die("MySQL konexioan errorea: ". mysqli_connect_error());
+		include 'deskonexioa.php';
+	}
+?>
